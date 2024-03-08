a71a312f6dc4c9650a04daa6adc9c007315f9d76
othniel sam faustino <faustinoothniel23@gmail.com>
2024-03-08 21:44:17 +0800

    write_your_first_flutter_app_faustino_othniel_sam

---
+++ wmad_308_flutter/faustino_othniel_sam_test_drive/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\SAM\\Desktop\\wmad_308_309\\wmad_308_flutter\\faustino_othniel_sam_test_drive" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\flutter"
+  "PROJECT_DIR=C:\\Users\\SAM\\Desktop\\wmad_308_309\\wmad_308_flutter\\faustino_othniel_sam_test_drive"
+  "FLUTTER_ROOT=C:\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\SAM\\Desktop\\wmad_308_309\\wmad_308_flutter\\faustino_othniel_sam_test_drive\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\SAM\\Desktop\\wmad_308_309\\wmad_308_flutter\\faustino_othniel_sam_test_drive"
+  "FLUTTER_TARGET=C:\\Users\\SAM\\Desktop\\wmad_308_309\\wmad_308_flutter\\faustino_othniel_sam_test_drive\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC8wNDgxN2M5OWM5ZmQ0OTU2ZjI3NTA1MjA0ZjdlMzQ0MzM1ODEwYWVkLw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\SAM\\Desktop\\wmad_308_309\\wmad_308_flutter\\faustino_othniel_sam_test_drive\\.dart_tool\\package_config.json"
+)
