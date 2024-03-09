a52df00f94bff9cf2e6763b4224dd6746e6fcfad
harlon_melchor <kingexile2k1@gmail.com>
2024-03-09 20:36:46 +0800

    animating_player_sprites_melchor_harlon

---
+++ wmad_309_animating_sprites/animating_player_melchor_harlon/game.js
@@ -0,0 +1,20 @@
+import Player from "./player.js";
+
+class Game {
+    constructor(ctx, width, height) {
+        this.ctx = ctx;
+        this.width = width;
+        this.height = height;
+
+        this.player = new Player(ctx,100,75);
+    }
+
+    draw() {
+        this.ctx.fillStyle = "green";
+        this.ctx.fillRect(0, 0, this.width, this.height);
+        
+        this.player.draw();
+    }  
+}
+
+export default Game; // alowing game to be used at index.html
