cbe3123ef4fef06d7355bf5b47a3a331531e0624
kikilao_ebenezer <ebenezerkikilao@gmail.com>
2024-03-09 11:56:19 +0800

    animating_sprites

---
+++ wmad_309_animating_sprites/game.js
@@ -0,0 +1,16 @@
+class Game {
+    constructor(ctx, width, height) {
+        this.ctx = ctx;
+        this.width = width;
+        this.height = height;
+
+        this.player 
+    }
+    draw() {
+
+
+        this.ctx.fillColor = "red";
+        this.ctx.rect(0, 0, 100, 100)
+    }
+}
+export default Game;
