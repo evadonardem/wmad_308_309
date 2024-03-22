eac796e9482f41caf01b8019688a7653ef170810
othniel sam faustino <faustinoothniel23@gmail.com>
2024-03-22 13:08:31 +0800

    wmad_309_animating_sprites_faustino_othniel_sam

---
+++ wmad_309_animating_sprites/animating_player_medrano_dave/player.js
@@ -0,0 +1,38 @@
+class Player {
+    constructor(ctx, width, height){
+        this.ctx = ctx;
+        this.width = width;
+        this.height = height;
+        this.currentFrame = 0;
+
+        
+        this.actionFrames = {
+            '00_idle': { 
+                'frameCount': 11,
+                'frames': [],
+            },
+            
+        };
+
+    for (const key in this.actionFrames){
+        if(Object.hasOwnProperty.call(this.actionFrames,key)) {
+            const action = this.actionFrames[key];
+            Array.from({length: action.frameCount},(_, k) => {
+                const image = new Image();
+                const folder = `player/${key}/`; 
+                const fileName = `skeleton-${key}_${(k < 10) ? `0${k}` : k}.png`;
+                const path = `${folder}${fileName}`;
+                image.src = path;
+                action.frames[k] = image;
+            });
+        }
+    }
+}
+
+    draw(){
+        this.currentFrame = this.currentFrame < this.actionFrames["00_idle"].frameCount
+            ? this.currentFrame : 0;
+        this.ctx.drawImage(this.actionFrames["00_idle"].frames[this.currentFrame++], 200, 175, this.width, this.height);
+    }
+}
+export default Player;
